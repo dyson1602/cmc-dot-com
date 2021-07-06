b4bad1033366132b1b3c6695914579a5ff6fe089
dyson1602 <cclark000@citymail.cuny.edu>
2021-07-06 16:02:13 -0400

    build custom form hook

--- src/components/ContactContainer.tsx
+++ src/components/ContactContainer.tsx
@@ -1,7 +1,30 @@
-import '../styles/contact-container.css'
+import '../styles/contact-container.css';
+import { useState } from 'react';
+
+interface EmailProps {
+  name: string;
+  email: string;
+  message: string;
+}
 
 export const ContactContainer: React.FC = () => {
+  const [emailProps, setEmailProps] = useState<EmailProps>();
+  const [name, setFormName] = useState<string>()
+
+  const formSubmitHandler = () => {};
+
   return (
-    <section id="contact">Contact</section>
-  )
-}
+    <section id="contact">
+      <h1>Contact</h1>
+      <form>
+        <label>Name: </label>
+        <input type="text" name="name" value={name} />
+        <label>Email: </label>
+        <input type="text" name="email" />
+        <label>Message:</label>
+        <textarea name="message" />
+        <input type="submit" value="Submit"/>
+      </form>
+    </section>
+  );
+};
