b0a54a5dc204d40731341ddefb060eafbb0dab42
dyson1602 <cclark000@citymail.cuny.edu>
2021-07-09 17:46:49 -0400

    fix scroll to top button functionality

--- src/components/ScrollToTopButton.tsx
+++ src/components/ScrollToTopButton.tsx
@@ -1,11 +1,13 @@
-import '../styles/scroll-to-top-button.css'
+import '../styles/scroll-to-top-button.css';
 
 export const ScrollToTopButton: React.FC = () => {
-  const landing = document.querySelector('#landing');
-
   return (
-    <button className="scroll-to-top"
-      onClick={() => landing && landing.scrollIntoView({ behavior: 'smooth' })}
+    <button
+      className="scroll-to-top"
+      onClick={() => {
+        const landing = document.querySelector('#landing');
+        landing && landing.scrollIntoView({ behavior: 'smooth' });
+      }}
     >
       Return to Top
     </button>
