c79b1c4bb540dcf2a66867dbbba327e48470acb3
dyson1602 <cclark000@citymail.cuny.edu>
2021-07-10 16:49:37 -0400

    define styles with icons for navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,29 @@
 import '../styles/nav-bar.css';
+import { DiGithubBadge } from 'react-icons/di';
+import { FaLinkedin, FaTwitter } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
+
 import { NavBarButton } from './NavBarButton';
 
-
 export const NavBar: React.FC = () => {
-  
-  const clickAction = (dest: string) => {
-    console.log('clicked')
-  }
-
   return (
     <nav id="nav-bar" className="sticky">
       <div className="link-wrapper">
         <NavBarButton dest="#bio" title="Bio" />
+
         <NavBarButton dest="#projects" title="Projects" />
+
         <NavBarButton dest="#tech-stack" title="Tech" />
-        <NavBarButton dest="#contact" title="Contact" />
+
+        <NavBarButton dest=".contact" title="Contact" />
+
+        <div className="link-wrapper-right">
+          <IconContext.Provider value={{ color: 'white', size: '35px' }}>
+            <DiGithubBadge />
+            <FaLinkedin />
+            <FaTwitter />
+          </IconContext.Provider>
+        </div>
       </div>
     </nav>
   );
--- src/components/NavBarButton.tsx
+++ src/components/NavBarButton.tsx
@@ -1,16 +1,21 @@
+import '../styles/nav-bar-button.css';
+
 interface NavBarButtonProps {
   dest: string;
   title: string;
 }
 
 export const NavBarButton: React.FC<NavBarButtonProps> = ({ dest, title }) => {
-  
   const clickAction = (dest: string) => {
     const target = document.querySelector(dest);
     if (target) {
-      target.scrollIntoView({behavior: 'smooth', block: 'start'})
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   };
 
-  return <button className="nav-bar-button" onClick={() => clickAction(dest)}>{title}</button>;
+  return (
+    <div className="nav-bar-button" onClick={() => clickAction(dest)}>
+      {title}
+    </div>
+  );
 };
